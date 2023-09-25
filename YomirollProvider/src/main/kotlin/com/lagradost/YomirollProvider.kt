d6b5cf34f4cbba1932c72275149fb2da40e11f69
imper1aldev <imperialdev@outlook.com>
2023-09-24 22:41:30 -0600

    Cambio yomiroll

--- YomirollProvider/src/main/kotlin/com/lagradost/YomirollProvider.kt
+++ YomirollProvider/src/main/kotlin/com/lagradost/YomirollProvider.kt
@@ -76,7 +76,7 @@ class YomirollProvider : MainAPI() {
         val url = request.data.replace("{start}", start)
         val position = request.data.toHttpUrl().queryParameter("start")?.toIntOrNull() ?: 0
 
-        val parsed = app.get(url, interceptor = tokenInterceptor).parsed<AnimeResult>()
+        val parsed = app.get(url, headers = getCrunchyrollToken()).parsed<AnimeResult>()
         val hasNextPage = position + 36 < parsed.total
 
         val home = parsed.data.map {
@@ -126,7 +126,7 @@ class YomirollProvider : MainAPI() {
             } else {
                 "$crApiUrl/cms/movie_listings/${anime.id}?locale=en-US"
             },
-            interceptor = tokenInterceptor
+            headers = getCrunchyrollToken()
         ).parsed<AnimeResult>().data.first()
 
         val title = anime.title
@@ -179,7 +179,7 @@ class YomirollProvider : MainAPI() {
             } else {
                 "$crApiUrl/cms/movie_listings/${anime.id}/movies"
             },
-            interceptor = tokenInterceptor
+            headers = getCrunchyrollToken()
         ).parsed<SeasonResult>()
 
         val chunkSize = Runtime.getRuntime().availableProcessors()
@@ -213,7 +213,7 @@ class YomirollProvider : MainAPI() {
     private suspend fun getEpisodes(seasonData: SeasonResult.Season): List<Episode> {
         val episodes = app.get(
             "$crApiUrl/cms/seasons/${seasonData.id}/episodes",
-            interceptor = tokenInterceptor
+            headers = getCrunchyrollToken()
         ).parsed<EpisodeResult>()
         return episodes.data.sortedBy { it.episode_number }.mapNotNull EpisodeMap@{ ep ->
             Episode(
@@ -281,7 +281,7 @@ class YomirollProvider : MainAPI() {
             val (mediaId, audioL) = it
             val streams = app.get(
                 "https://beta-api.crunchyroll.com/content/v2/cms/videos/$mediaId/streams",
-                interceptor = tokenInterceptor,
+                headers = getCrunchyrollToken(),
                 timeout = 20000
             ).parsedSafe<CrunchyrollSourcesResponses>()
 
